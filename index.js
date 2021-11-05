4b7b1f08648b8561eddfd26c854d9a6206835809
or <useyourtimewell@gmail.com>
2021-11-05 15:26:58 +1300

    Upgraded function to 2.0 where it removes excess spaces

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+/*
+// FIRST ATTEMPT
 const makeStringOnly = (string) => {
   let formattedString = string.replace(/[!,?,-,;,%]/g, " ");
   return formattedString;
 };
 
 makeStringOnly("hello%world");
-
-// TEST CASES: “hello world”, “hello%world”, “!hello world”, “;hello?world!”, “hello;world” and “hello world?”.
-// I added in the "%"" even though not in the mission
 module.exports = makeStringOnly;
+// TEST CASES: “hello world”, “hello%world”, “!hello world”, “;hello?world!”, “hello;world” and “hello world?”.
+*/
+
+// FINAL newMakeStringOnly
+// Bonus functionality: If string has a special characted at the beginning or end of the string,
+// or if there is a special character in the middle surrounded by a space, then remove special character only and do not put space.
+const newMakeStringOnly = (string) => {
+  let temp = string.replace(/[!,?,-,;,%]/g, " ");
+  let newFormattedString = temp.replace(/\s+/g, " ").trim();
+  return newFormattedString;
+};
+
+newMakeStringOnly("  !!!Hello!new     !!!!!!!    ;;;%world.");
+module.exports = newMakeStringOnly;
--- index.test.js
+++ index.test.js
@@ -1,25 +1,25 @@
-const makeStringOnly = require("./index");
+const newMakeStringOnly = require("./index");
 
 test("Converts any odd characters to spaces", () => {
-  expect(makeStringOnly("hello!world")).toBe("hello world");
+  expect(newMakeStringOnly("hello!world")).toBe("hello world");
 });
 
 test("Converts any odd characters to spaces", () => {
-  expect(makeStringOnly("hello%world")).toBe("hello world");
+  expect(newMakeStringOnly("hello%world")).toBe("hello world");
 });
 
 test("Converts any odd characters to spaces", () => {
-  expect(makeStringOnly("!hello world")).toBe(" hello world");
+  expect(newMakeStringOnly("!hello world")).toBe("hello world");
 });
 
 test("Converts any odd characters to spaces", () => {
-  expect(makeStringOnly(";hello?world!")).toBe(" hello world ");
+  expect(newMakeStringOnly(";hello?world!")).toBe("hello world");
 });
 
 test("Converts any odd characters to spaces", () => {
-  expect(makeStringOnly("hello;world")).toBe("hello world");
+  expect(newMakeStringOnly("hello;world")).toBe("hello world");
 });
 
 test("Converts any odd characters to spaces", () => {
-  expect(makeStringOnly("hello world?")).toBe("hello world ");
+  expect(newMakeStringOnly("hello world?")).toBe("hello world");
 });
