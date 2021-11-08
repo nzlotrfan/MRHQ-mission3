a357da663d923ee6559f41c4343da95bee0dbce1
or <useyourtimewell@gmail.com>
2021-11-08 23:59:36 +1300

    updated front end display

---
+++ car-search-app/src/components/Search.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import "./Search.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import turners from "../assets/Turners-Logo.gif";
+function Search() {
+  const [searchString, setSearchString] = useState("");
+  const [displayedFormattedSearchString, setDisplayedFormattedSearchString] =
+    useState("");
+  const [resultsTitle, setResultsTitle] = useState("");
+  const [resultsText, setResultsText] = useState("");
+
+  const handleSubmit = () => {
+    // Remove Special Characters function
+    const newMakeStringOnly = (string) => {
+      let temp = string.replace(/[!,?,-,;,%]/g, " ");
+      let formattedString = temp.replace(/\s+/g, " ").trim();
+      setDisplayedFormattedSearchString(
+        `You searched for: "${formattedString}"`
+      );
+      return formattedString;
+    };
+
+    function addClass() {
+      document
+        .getElementById("search-results")
+        .classList.add("search-results-border");
+    }
+    addClass();
+    const formattedString = newMakeStringOnly(searchString);
+    axios
+      .post("http://localhost:4000/search", {
+        searchString: formattedString,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setSearchString("");
+          const rawData = response.data;
+          const question = rawData.result.results[0].question;
+          const answer = rawData.result.results[0].text;
+          setResultsTitle(question);
+          setResultsText(answer);
+          console.log(rawData);
+        }
+      })
+      .catch(() => {
+        setSearchString("");
+        setResultsTitle("");
+      });
+  };
+
+  return (
+    <div className="App">
+      <img className="logo" src={turners} alt="logo" />
+      {/* <h1>Turners Search Page</h1> */}
+      <div className="search-box">
+        <TextField
+          variant="outlined"
+          size="small"
+          type="text"
+          placeholder="Search me"
+          value={searchString}
+          onChange={(e) => setSearchString(e.target.value)}
+        />
+        <div className="btn">
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+      </div>
+      <div className="search-results-container">
+        <div id="search-results">
+          <div className="search-query">{displayedFormattedSearchString}</div>
+          <div className="results-title">{resultsTitle}</div>
+          <div className="results-text">{resultsText}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
--- ibmSearch/server.js
+++ ibmSearch/server.js
@@ -1,56 +1,47 @@
-/*
-const DiscoveryV2 = require("ibm-watson/discovery/v2");
-const { IamAuthenticator } = require("ibm-watson/auth");
-
-const discovery = new DiscoveryV2({
-  version: "{2021-10-30}",
-  authenticator: new IamAuthenticator({
-    apikey: "{AADtvKP67919k2JyciexWvSFLS5vIC8Cj-c6FaZ6YPlN}",
-  }),
-  serviceUrl: "https://api.eu-gb.discovery.watson.cloud.ibm.com",
-  headers: {
-    "X-Watson-Learning-Opt-Out": "true",
-  },
-});
-
-discovery.method(params).catch((err) => {
-  console.log("error:", err);
-});
-
-discovery
-  .methodName(parameters)
-  .then((response) => {
-    console.log(response.headers);
-  })
-  .catch((err) => {
-    console.log("error:", err);
-  });
-*/
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const DiscoveryV1 = require("ibm-watson/discovery/v1");
+const { IamAuthenticator } = require("ibm-watson/auth");
 app.use(express.urlencoded({ extended: true })); // we're telling the server to expect data inputed from an HTML form. But if we're sending the data via JSON then we don't actually need this line
 app.use(express.json()); // tells the server to accept JSON data
 app.use(cors()); // tells the server to accept incoming data from other locations
 
-const user = {
-  email: "hi@hi.com",
-  password: "password123",
-};
+// DISCOVERY API START
+const discovery = new DiscoveryV1({
+  version: "2021-11-07",
+  authenticator: new IamAuthenticator({
+    apikey: "SNA6_eabsAwkXFlvf3Vf1jR--eKPUxvTta-wrDnUneGs",
+  }),
+  serviceUrl: "https://api.eu-gb.discovery.watson.cloud.ibm.com",
+});
 
 // when you want to access specific information inside of the request. All of the data that you send from the frontend ..
 // .. will be accessible via req.body
 app.post("/search", function (req, res) {
   console.log(req.body);
-  if (req.body.email === user.email && req.body.password === user.password) {
-    res.status(200).send("Search succeeded (sent from backend)");
-  } else {
-    res.status(401).send("Search failed (sent from backend)");
-  }
+  // Watson Query data START
+  const userQuery = req.body.searchString; // TEST Search parameters
+
+  const queryParams = {
+    environmentId: "d092ad96-249e-43af-a81d-8eef1a4aa9ab",
+    collectionId: "18126044-5588-4be0-bf30-2fcbc87c45f3",
+    query: userQuery,
+  };
+
+  discovery
+    .query(queryParams)
+    .then((queryResponse) => {
+      // console.log(JSON.stringify(queryResponse, null, 2));
+      const searchRes = JSON.stringify(queryResponse, null, 2);
+      res.status(200).send(searchRes);
+    })
+    .catch((err) => console.log(err));
+
+  // Watson Query data END
 });
 
+// FAILED 404 page
 // app.get("*", function (req, res) {
 //   res.send("This page doesn't exist yo!");
 // });
