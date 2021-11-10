7bdae206c61131c905180e0f1a9a7147c25daa60
or <useyourtimewell@gmail.com>
2021-11-10 21:21:21 +1300

    Cleaned up the file structure

--- old/index.js
+++ old/index.js
@@ -14,7 +14,7 @@ module.exports = makeStringOnly;
 // Bonus functionality: If string has a special characted at the beginning or end of the string,
 // or if there is a special character in the middle surrounded by a space, then remove special character only and do not put space.
 const newMakeStringOnly = (string) => {
-  let temp = string.replace(/[!,?,-,;,%]/g, " ");
+  let temp = string.replace(/[!,?,\-,;,%]/g, " ");
   let newFormattedString = temp.replace(/\s+/g, " ").trim();
   return newFormattedString;
 };
